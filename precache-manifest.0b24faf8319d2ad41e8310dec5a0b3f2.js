a846ba1467222df95c60026304143325e798643b
Ivan Franco <ifranco@tupaca.com>
2019-02-04 14:05:29 -0300

    Updates

---
+++ precache-manifest.0b24faf8319d2ad41e8310dec5a0b3f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b83cafe312f16519f3f8",
+    "url": "/react-tutorial/static/css/main.cec1aabe.chunk.css"
+  },
+  {
+    "revision": "b83cafe312f16519f3f8",
+    "url": "/react-tutorial/static/js/main.b83cafe3.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "f630750815cf86607bd7181b41acf9be",
+    "url": "/react-tutorial/index.html"
+  }
+];
